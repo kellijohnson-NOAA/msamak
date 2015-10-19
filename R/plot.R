1462741d9ca4c4fc41452f338b370f909b60d525
kellijohnson <kellifayejohnson@gmail.com>
2015-10-19 11:03:01 -0700

    Add changes to plot

--- R/plot.R
+++ R/plot.R
@@ -1,6 +1,7 @@
 
-plotts <- function(folder = "100", value = "pred_rec",
-  speciesnames = c("pollock", "mackerel", "cod")) {
+plotts <- function(folder = "sample", value = "pred_rec",
+  speciesnames = c("pollock", "mackerel", "cod"),
+  text = NULL) {
 
 equalts <- function(list, folder) {
   list <- lapply(list, function(x) {
@@ -12,7 +13,7 @@ equalts <- function(list, folder) {
     })
   do.call("rbind", list)
 }
-# browser()
+
 x <- do.call("rbind", lapply(folder, function(x) {
   keep <- c(ls(), "xx")
   source(file.path(x, "ms.rep"), local = TRUE)
@@ -23,14 +24,14 @@ x <- do.call("rbind", lapply(folder, function(x) {
   return(xx)
 }))
 
-ggplot(data = x,
+g <- ggplot(data = x,
   aes(x = time, y = val)) +
   geom_point(data = x, aes(x = time, y = val)) +
   facet_grid(pos ~ folder, scales = "free_y") +
   xlab("year") + ylab(gsub("_", " ", substitute(value))) +
   theme_bw() +
   geom_vline(lty = 2, xintercept =
-    get("styr", envir = envom) - get("styr_pred", envir = envom)) +
+    get("styr", envir = om) - get("styr_pred", envir = om)) +
   theme(plot.background = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -40,17 +41,17 @@ ggplot(data = x,
         legend.title = element_text(size = 7, face = "bold"),
         legend.text = element_text(size = 7, face = "bold"),
         plot.title = element_text(lineheight = 0.8, size = 10)
-  )
-  # geom_text(aes(x, y, label = lab),
-  #   data = data.frame(x = 35, y = Inf,
-  #   lab = round(c(get("steepness", envir = envom),
-  #           get("steepness", envir = envsample),
-  #           get("steepness", envir = env100)), digits = 3),
-  #   pos = rep(speciesnames, 3),
-  #   folder = rep(folder, each = 3)), vjust = 2.5) +
-  # ggtitle("Estimated recruitment across models (column: OM, true sample sizes, 100 every year)
-  #   and species (row). Dashed line indicates start of data and steepness is printed in right corner.")
-
+   )
+if (!is.null(text)) {
+  g <- g +
+  geom_text(aes(x, y, label = lab),
+    data = data.frame(x = 35, y = Inf,
+    lab = round(c(get("steepness", envir = om),
+    get("steepness", envir = sample)), digits = 3),
+    pos = rep(speciesnames, length(folder)),
+    folder = rep(folder, each = 3)), vjust = 2.5)
 }
-# plotts(folder = c("model", "sample", "100"), value = "Sp_Biom")
-# plotts(folder = c("model", "sample", "100"), value = "pred_rec")
+  g
+}
+# plotts(folder = c("om"), value = "Sp_Biom")
+# plotts(folder = c("om", "sample"), value = "pred_rec", text = TRUE)
